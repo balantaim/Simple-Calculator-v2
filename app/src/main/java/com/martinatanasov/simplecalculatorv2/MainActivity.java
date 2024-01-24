112a0332e7e0f969f4e31e201a7fec66620157c9
Balantaim <martin.ned.atanasov@gmail.com>
2024-01-24 20:51:44 +0200

    Stage 1.9.13 added equal animation, target api 34, some small improvements

--- app/src/main/java/com/martinatanasov/simplecalculatorv2/MainActivity.java
+++ app/src/main/java/com/martinatanasov/simplecalculatorv2/MainActivity.java
@@ -24,7 +24,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
